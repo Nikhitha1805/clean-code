45727a196feb03e800dc1246d366ab7af49ce219
Nikhitha1805 <68781282+Nikhitha1805@users.noreply.github.com>
2020-07-25 20:40:39 +0530

    Add files via upload

---
+++ house.java
@@ -0,0 +1,93 @@
+class Area
+
+{
+
+    private double s;
+
+    private double a;
+ 
+   public void setSqfeet(double s)
+
+    {
+
+        this.s=s;
+
+    }
+
+    public double getSqfeet(double s)
+
+    {
+
+        return this.s;
+  
+  }
+
+    public void setArea(double a)
+
+    {
+
+        this.a=a;
+ 
+   }
+
+    public double getArea()
+ 
+   {
+
+        if(this.s==1200)
+   
+     {
+
+            return (this.a)/(1200*this.s);
+ 
+       }
+ 
+       else if(this.s==1500)
+   
+     {
+ 
+           return (this.a)/(1500*this.s);
+
+        }
+    
+    else if(this.s==1800)
+    
+    {
+     
+       return (this.a)/(1800*this.s);
+  
+      }
+
+        else 
+   
+     {
+     
+      return (this.a)/(2500*this.s);
+ 
+        }
+ 
+   }
+
+}
+
+public class Main
+
+{
+	
+public static void main(String[] args)
+
+{
+
+Area a=new Area();
+  
+      a.setSqfeet(1500);
+
+        a.setArea(2589000);
+ 
+       System.out.println(a.getArea());
+ 
+       //System.out.println(a.getSqfeet());
+	
+}
+
+}
---
+++ si.java
@@ -0,0 +1,71 @@
+class Interest
+
+{  private int p,t;
+ 
+  private double r;
+ 
+   public void setPrinciple(int p )
+  
+  {
+      
+  this.p=p ;
+ 
+   }
+ 
+   public void setRate(double r)
+ 
+   {
+      
+  this.r=r;
+
+    }
+  
+  public void setTime(int t)
+ 
+   {
+     
+   this.t=t;
+
+    }
+ 
+   public int getPrinciple()
+ 
+   {
+
+        return this.p;
+   
+ }
+
+    public double getRate()
+  
+  {
+
+        return this.r;
+
+    }
+
+    public int getTime()
+ 
+   {
+
+        return this.t;
+
+    }
+
+    public double simpleIntrest()
+
+    {
+
+        return ((this.p)*(this.r)*(this.t))/100;
+   
+    }
+
+    public double compoundInterest()
+
+    {
+ 
+       return this.p*(Math.pow((1+this.r),this.t)-1);
+    
+    }
+
+}
